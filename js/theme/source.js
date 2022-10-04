d8fa2ea65eee60fcd901d8753368ca7f7160e159
murtazajoo <jmtariq2003@gmail.com>
2022-10-04 14:27:00 +0530

    checkbox toggle added

--- js/theme/source.js
+++ js/theme/source.js
@@ -1,15 +1,15 @@
 
 let themes = localStorage.getItem('theme');
-
-document.getElementById('logo').addEventListener('click', () => {
+let themeBox = document.getElementById('checkbox')
+themeBox.addEventListener('change', () => {
   let themes = localStorage.getItem('theme');
 
-  if (themes == 'light') {
-    localStorage.setItem('theme', "dark");
+  if (themes == 'dark') {
+    localStorage.setItem('theme', "light");
     theme()
   }
   else {
-    localStorage.setItem('theme', "light");
+    localStorage.setItem('theme', "dark");
     theme()
   }
 })
@@ -20,9 +20,11 @@ function theme() {
   if (themes == "dark") {
     document.documentElement.style.setProperty('--main', '#fff');
     document.documentElement.style.setProperty('--secondary', '#36454F');
+    themeBox.checked = "checked"
 
   } else {
     document.documentElement.style.setProperty('--main', '#02343a');
     document.documentElement.style.setProperty('--secondary', '#53d0de6d');
   }
 }
+theme()
