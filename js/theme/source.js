665436decb62791c0550f330479a1a5a156efad3
murtazajoo <jmtariq2003@gmail.com>
2022-10-04 15:21:06 +0530

    yt

--- js/theme/source.js
+++ js/theme/source.js
@@ -14,21 +14,26 @@ themeBox.addEventListener('change', () => {
   }
 })
 
+let btns = document.querySelectorAll(".grid a")
 function theme() {
   let themes = localStorage.getItem('theme');
 
   if (themes == "dark") {
     document.documentElement.style.setProperty('--main', '#F9F6EE');
     document.documentElement.style.setProperty('--secondary', '#36454F');
-    document.querySelectorAll('.box')[0].style.backgroundColor = "rgba(0, 0, 0, 0.598)"
-    document.querySelectorAll('.box')[1].style.backgroundColor = "rgba(0, 0, 0, 0.598)"
+    document.querySelectorAll('.box')[0].style.backgroundColor = "#36454F"
+    document.querySelectorAll('.box')[1].style.backgroundColor = "#36454F"
     themeBox.checked = "checked"
+
+    btns.forEach((btn) => { btn.style.backgroundColor = "teal" })
 
   } else {
     document.documentElement.style.setProperty('--main', '#02343a');
     document.documentElement.style.setProperty('--secondary', '#53d0de6d');
     document.querySelectorAll('.box')[0].style.backgroundColor = "rgba(255, 255, 255, 0.498)"
     document.querySelectorAll('.box')[1].style.backgroundColor = "rgba(255, 255, 255, 0.498)"
+    btns.forEach((btn) => { btn.style.backgroundColor = "rgb(119, 200, 200)" })
+
   }
 }
 
