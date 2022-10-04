8ea6a7ee88eea7b95dd227a7c905f6f7b1967705
murtazajoo <jmtariq2003@gmail.com>
2022-10-04 15:02:44 +0530

    dfs

--- js/theme/source.js
+++ js/theme/source.js
@@ -18,13 +18,17 @@ function theme() {
   let themes = localStorage.getItem('theme');
 
   if (themes == "dark") {
-    document.documentElement.style.setProperty('--main', '#fff');
+    document.documentElement.style.setProperty('--main', '#F9F6EE');
     document.documentElement.style.setProperty('--secondary', '#36454F');
+    document.querySelectorAll('.box')[0].style.backgroundColor = "rgba(0, 0, 0, 0.598)"
+    document.querySelectorAll('.box')[1].style.backgroundColor = "rgba(0, 0, 0, 0.598)"
     themeBox.checked = "checked"
 
   } else {
     document.documentElement.style.setProperty('--main', '#02343a');
     document.documentElement.style.setProperty('--secondary', '#53d0de6d');
+    document.querySelectorAll('.box')[0].style.backgroundColor = "rgba(255, 255, 255, 0.498)"
+    document.querySelectorAll('.box')[1].style.backgroundColor = "rgba(255, 255, 255, 0.498)"
   }
 }
 
